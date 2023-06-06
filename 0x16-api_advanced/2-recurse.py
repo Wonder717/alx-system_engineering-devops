204f062154a332f9169361df3db8b9f38a290056
Wonder Agudah <85680026+Wonder717@users.noreply.github.com>
2023-06-06 15:17:30 +0000

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,48 +1,31 @@
 #!/usr/bin/python3
-"""
-Queries using recursive
-"""
+"""Contains recurse function"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts
-    recursively
-    Args:
-        subreddit(string): name of the subreddit
-        hot_list(list): list of current hotlist
-        count(int): number of current count
-    Returns:
-        hot_list
-    """
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    params = {"after": after, "count": count, "limit": 100}
-    headers = {"User-Agent": "Safari 12.1"}
-
-    data = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    if data.status_code == 404:
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
 
-    res = data.json().get("data")
-    after = res.get("after")
-    count += res.get("dist")
-
-    for c in res.get("children"):
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
         hot_list.append(c.get("data").get("title"))
 
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
-    return hot_list
-
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument ")
-    else:
-        result = recurse(sys.argv[1])
-        if result is not None:
-            print(len(result))
-        else:
-            print("None")
+    return 
